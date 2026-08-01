f0fddb6e9abb55a4d65811595daf4666070fd9ec
Ikram Awol <99601469+ikramawol@users.noreply.github.com>
2026-08-01 08:42:48 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "habit-tracker-v4";
+const CACHE_NAME = "habit-tracker-v5";
 const ASSETS = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", (event) => {
